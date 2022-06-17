8c90ab3961cf40aa562d44829f13bb851141a9f4
wei_gu <wei_gu@intsig.net>
2022-06-17 14:12:31 +0800

    feat: menu 菜单

---
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 一级菜单 -->
+  <el-menu background-color="#304156" text-color="#fff" active-text-color="#ffd04b">
+    <!-- 子集 menu 菜单 -->
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item index="1-1"
+        ><el-icon><Eleme /></el-icon>item one</el-menu-item
+      >
+      <el-menu-item index="1-2"
+        ><el-icon><Eleme /></el-icon>item one</el-menu-item
+      >
+      <el-menu-item index="1-3"
+        ><el-icon><Eleme /></el-icon>item three</el-menu-item
+      >
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><Promotion /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.el-menu {
+  border: none;
+}
+</style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>sidebar</div>
+  <div class="sidebar">
+    <div class="title">gavin-admin</div>
+    <el-scrollbar>
+      <SidebarMenu></SidebarMenu>
+    </el-scrollbar>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SidebarMenu from './SidebarMenu.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sidebar {
+  text-align: center;
+  box-sizing: border-box;
+  .title {
+    padding: 20px;
+  }
+}
+</style>
