f79b3707c10319d2c342e8bd3d6fef3f8ad86ecc
wei_gu <wei_gu@intsig.net>
2022-06-20 15:21:34 +0800

    feat: vue-i18n

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed, watch } from 'vue'
+import { useStore } from 'vuex'
 
-<style lang="scss"></style>
+// 引入element-plus中文、英文语言包
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+
+// 定义store变量
+const store = useStore()
+// 定义语言切换变量
+const locale = computed(() => (store.getters.language === 'zh' ? zhCn : en))
+
+watch(locale, newvalue => {
+  console.log(newvalue)
+})
+</script>
+<style lang="less"></style>
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,19 +1,24 @@
 import { createI18n } from 'vue-i18n'
+import { getItem } from '@/utils/storage'
+import { LANG } from '@/constant'
+import mZhLocale from './lang/zh'
+import mEnLocale from './lang/en'
 
 const messages = {
   en: {
     msg: {
-      test: 'hello world'
+      ...mEnLocale
     }
   },
   zh: {
     msg: {
-      test: '你好世界'
+      ...mZhLocale
     }
   }
 }
 
-const locale = 'zh'
+// 创建locale语言变量
+const locale = getItem(LANG) || 'zh'
 
 const i18n = createI18n({
   // 使用 Composition Api模式， 需要将其设置为false
