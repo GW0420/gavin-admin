e62152d1a7f7660dd1dc10a1f9fb075699ca25fa
wei_gu <wei_gu@intsig.net>
2022-06-21 14:15:56 +0800

    feat: headersearch

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header-search">
-    <svg-icon icon="search" class="icons" @click="onShowSearch"></svg-icon>
+    <svg-icon icon="search" class="icons" @click.stop="onShowSearch"></svg-icon>
     <el-select
       ref="headerSearchSelectRef"
       :class="[isShow ? 'header-show-search' : 'header-hiddle-search']"
@@ -23,18 +23,34 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { filterRouters } from '@/utils/route'
 import Fuse from 'fuse.js'
 import { generateRoutes } from './components/FuseData'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 控制 search显示
-const isShow = ref(true)
+const isShow = ref(false)
 const onShowSearch = () => {
   isShow.value = !isShow.value
   headerSearchSelectRef.value.focus()
 }
+
+// 关闭 search 的处理事件
+const onClose = () => {
+  headerSearchSelectRef.value.blur()
+  isShow.value = false
+  searchOptions.value = []
+}
+// 监听 search 打开，处理 close 事件
+watch(isShow, val => {
+  if (val) {
+    document.body.addEventListener('click', onClose)
+  } else {
+    document.body.removeEventListener('click', onClose)
+  }
+})
 
 // search
 const search = ref('')
@@ -59,32 +75,43 @@ const onSelectChange = value => {
 }
 
 // 检索数据源
-const searchPool = computed(() => {
+let searchPool = computed(() => {
   const filterRoutes = filterRouters(router.getRoutes())
   return generateRoutes(filterRoutes)
 })
 
-console.log(searchPool.value)
-
 // 检索库相关
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
-  // name：搜索的键
-  // weight：对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
+let fuse
+const initFuse = searchPool => {
+  fuse = new Fuse(searchPool, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表。 这支持嵌套路径、加权搜索、在字符串和对象数组中搜索。
+    // name：搜索的键
+    // weight：对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool.value)
+
+// 处理国际化
+watchSwitchLang(() => {
+  searchPool = computed(() => {
+    const filterRoutes = filterRouters(router.getRoutes())
+    return generateRoutes(filterRoutes)
+  })
+  initFuse(searchPool.value)
 })
 </script>
 
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,10 +1,24 @@
+import i18n from '@/i18n'
+import store from '@/store'
+import { watch } from 'vue'
+
 /**
  * 国际化内容转换
  * @params title
  */
 
-import i18n from '@/i18n'
-
 export const generateTitle = title => {
   return i18n.global.t('msg.route.' + title)
+}
+
+/**
+ * cbs 所有的回掉
+ */
+export const watchSwitchLang = (...cbs) => {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach(cb => cb(store.getters.language))
+    }
+  )
 }
