560b36072167b288190f7153d9a1740ab10a0866
wei_gu <wei_gu@intsig.net>
2022-06-23 11:35:09 +0800

    feat: 权限列表弹窗

---
+++ src/views/role-list/components/RoleDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-dialog :title="$t('msg.excel.roleDialogTitle')" :model-value="showRoleDialog" @close="handleRoleCancel">
+    内容
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="handleRoleCancel">{{ $t('msg.universal.cancel') }}</el-button>
+        <el-button type="primary" @click="handleRoleConfirm">{{ $t('msg.universal.confirm') }}</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  showRoleDialog: {
+    type: Boolean,
+    required: true
+  }
+})
+const emits = defineEmits(['update:showRoleDialog'])
+
+/**
+  确定按钮点击事件
+ */
+const handleRoleConfirm = async () => {
+  closed()
+}
+
+/**
+ * 关闭
+ */
+const handleRoleCancel = () => {
+  emits('update:showRoleDialog', false)
+}
+</script>
--- src/views/role-list/index.vue
+++ src/views/role-list/index.vue
@@ -6,28 +6,34 @@
         <el-table-column :label="$t('msg.role.name')" prop="title"> </el-table-column>
         <el-table-column :label="$t('msg.role.desc')" prop="describe"> </el-table-column>
         <el-table-column :label="$t('msg.role.action')" prop="action" width="260">
-          <el-button type="primary" size="mini">
+          <el-button type="primary" size="mini" @click="handleShowRoleDialog">
             {{ $t('msg.role.assignPermissions') }}
           </el-button>
         </el-table-column>
       </el-table>
     </el-card>
+    <RoleDialog v-model:showRoleDialog="showRoleDialog"></RoleDialog>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import { RoleList } from '@/api/rbac'
 import { watchSwitchLang } from '@/utils/i18n'
+import RoleDialog from './components/RoleDialog'
 
+// 角色列表
 const roleList = ref([])
 const getRoleList = async () => {
   roleList.value = await RoleList()
 }
+getRoleList()
 
-onMounted(() => {
-  getRoleList()
-})
+// 设置权限
+const showRoleDialog = ref(false)
+const handleShowRoleDialog = () => {
+  showRoleDialog.value = true
+}
 
 // 监听语言变化
 watchSwitchLang(getRoleList)
